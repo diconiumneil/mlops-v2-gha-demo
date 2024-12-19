29b49be73af555885f8a5dfb48f47f2a445f79b1
sinclnei <neil.sinclair@diconium.com>
2024-12-19 15:17:56 +0100

    More tf updates

--- infrastructure/modules/resource-group/main.tf
+++ infrastructure/modules/resource-group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "adl_rg" {
-  name     = "rg-${var.prefix}-${var.postfix}${var.env}"
+  name     = "RG-NeilSinclair"
   location = var.location
   tags     = var.tags
 }
